edfc7d26bd0dc17a1f5aded7b2a42e3111c679c8
ayshanpackt <ayshan@packt.com>
2026-07-03 11:10:57 +0530

    Update playwright.config.ts

--- Chapter01/playwright.config.ts
+++ Chapter01/playwright.config.ts
@@ -1,1 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
 
+export default defineConfig({
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+  ],
+
+  use: {
+    trace: 'on',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+});
